39ba30749a609925473231983f7164cb38a06b3d
sdgsnehal <sdgsnehal@gmail.com>
2023-07-29 19:32:44 +0530

    ejs

--- 01-sharing-data-across-requests-and-users/app.js
+++ 01-sharing-data-across-requests-and-users/app.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars')
+
 
 const app = express();
-app.engine('hbs',expressHbs.engine({layoutsDir:'views/layout/',defaultLayout:'main-layout',extname:'hbs'}));
-app.set('view engine','hbs')
+
+app.set('view engine','ejs')
 app.set('views', 'views')
 
 const adminData = require('./routes/admin');
