b3d99caeccbcf611a6ff7494fe7acdd9c8798b89
sdgsnehal <sdgsnehal@gmail.com>
2023-07-29 12:10:02 +0530

    Handlebars

--- 01-sharing-data-across-requests-and-users/app.js
+++ 01-sharing-data-across-requests-and-users/app.js
@@ -2,13 +2,16 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars')
 
 const app = express();
-app.set('view engine','pug ')
+app.engine('hbs',expressHbs.engine({layoutsDir:'views/layout/',defaultLayout:'main-layout',extname:'hbs'}));
+app.set('view engine','hbs')
 app.set('views', 'views')
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { render } = require('pug');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,7 +20,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404',{pageTitle:'Page Not Found'})
 });
 
 app.listen(3000);
--- 01-sharing-data-across-requests-and-users/routes/shop.js
+++ 01-sharing-data-across-requests-and-users/routes/shop.js
@@ -8,8 +8,10 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log('shop.js', adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products
+  res.render('shop',{prods:products , pageTitle:'Shop',path:'/' ,hasProducts:products.length > 0,activeShop:true,productCSS:true,layout:false})
 });
 
 module.exports = router;
